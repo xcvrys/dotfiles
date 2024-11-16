198069b148e7301c34bc548ec88a32ed8a556340
xcvrys <xcvrys@gmail.com>
2024-11-16 13:06:09 +0100

    sdlyugfuiksddhl

--- nvim/lua/xcvrys/plugins/lsp.lua
+++ nvim/lua/xcvrys/plugins/lsp.lua
@@ -196,6 +196,51 @@ return {
 				filetypes = { "svelte" },
 				root_dir = lspconfig.util.root_pattern("svelte.config.js", "package.json", ".git"),
 			})
+
+			local keymap = vim.keymap
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					local opts = { buffer = ev.buf, silent = true }
+
+					opts.desc = "Show LSP references"
+					keymap.set("n", "<leader>gr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+
+					opts.desc = "Go to declaration"
+					keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts) -- go to declaration
+
+					opts.desc = "Show LSP definitions"
+					keymap.set("n", "<leader>gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+
+					opts.desc = "Show LSP implementations"
+					keymap.set("n", "<leader>gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+
+					opts.desc = "Show LSP type definitions"
+					keymap.set("n", "<leader>gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+
+					opts.desc = "Smart rename"
+					keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+
+					opts.desc = "Show buffer diagnostics"
+					keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+					opts.desc = "Show line diagnostics"
+					keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+					opts.desc = "Go to previous diagnostic"
+					keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+
+					opts.desc = "Go to next diagnostic"
+					keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+
+					opts.desc = "Show documentation for what is under cursor"
+					keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+
+					opts.desc = "Restart LSP"
+					keymap.set("n", "<leader>lsr", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+				end,
+			})
 		end,
 	},
 }
