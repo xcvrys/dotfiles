14201f94441753b4805c6d6c7e43ef028a2b085d
xcvrys <xcvrys@gmail.com>
2026-06-12 17:42:27 +0200

    chore(nvim): organize

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -175,49 +175,13 @@ return {
 		-- { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
 		-- { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
 		-- git
-		-- {
-		-- 	"<leader>gb",
-		-- 	function()
-		-- 		Snacks.picker.git_branches()
-		-- 	end,
-		-- 	desc = "Git Branches",
-		-- },
-		-- {
-		-- 	"<leader>gl",
-		-- 	function()
-		-- 		Snacks.picker.git_log()
-		-- 	end,
-		-- 	desc = "Git Log",
-		-- },
-		-- {
-		-- 	"<leader>gL",
-		-- 	function()
-		-- 		Snacks.picker.git_log_line()
-		-- 	end,
-		-- 	desc = "Git Log Line",
-		-- },
-		{
-			"<leader>gs",
-			function()
-				Snacks.picker.git_status()
-			end,
-			desc = "Git Status",
-		},
+		-- { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches", },
+		-- { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log", },
+		-- { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line", },
+		-- { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status", },
 		-- { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
-		-- {
-		-- 	"<leader>gd",
-		-- 	function()
-		-- 		Snacks.picker.git_diff()
-		-- 	end,
-		-- 	desc = "Git Diff (Hunks)",
-		-- },
-		-- {
-		-- 	"<leader>gf",
-		-- 	function()
-		-- 		Snacks.picker.git_log_file()
-		-- 	end,
-		-- 	desc = "Git Log File",
-		-- },
+		-- { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)", },
+		-- { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File", },
 		-- gh
 		-- { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
 		-- { "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
@@ -273,7 +237,7 @@ return {
 		-- { "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
 		-- { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
 		-- { "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
-		-- { "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
+		-- { "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks", },
 		{
 			"<leader>sM",
 			function()
@@ -284,7 +248,7 @@ return {
 		-- { "<leader>sp", function() Snacks.picker.lazy() end, desc = "Search for Plugin Spec" },
 		-- { "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
 		-- { "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
-		-- { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
+		-- { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History", },
 		{
 			"<leader>cs",
 			function()
@@ -293,7 +257,13 @@ return {
 			desc = "Colorschemes",
 		},
 		-- LSP
-		-- { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+		{
+			"gd",
+			function()
+				Snacks.picker.lsp_definitions()
+			end,
+			desc = "Goto Definition",
+		},
 		-- { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
 		-- { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
 		-- { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
