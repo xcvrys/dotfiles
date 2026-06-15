e71ea0571025ed760991a7672ed1e43fbc0584e0
xcvrys <xcvrys@gmail.com>
2026-06-15 18:25:27 +0200

    chore(fish): add nvim `mason` to path

--- fish/.config/fish/enviroment.fish
+++ fish/.config/fish/enviroment.fish
@@ -5,3 +5,8 @@ set -gx PNPM_HOME "/home/xcvrys/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
+
+set -l mason_bin "$HOME/.local/share/nvim/mason/bin"
+if not string match -q -- $mason_bin $PATH
+  set -gx PATH "$mason_bin" $PATH
+end
